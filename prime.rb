f2657bff6cb67951cdab1d04253ce4289bf6564b
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 15:23:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(n)
   i = 2
   result = true
   n *= -1 if n < 0 
-  return false if n == 1 
+  return false if n == 1 || n == 0
   while i < n / 2 #ideally while i < sqrt of n
     n % i == 0 ? (return false) : i += 1
   end
