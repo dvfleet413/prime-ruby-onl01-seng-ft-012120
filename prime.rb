6f5d40cf4cbaab0bccdb2d0bb48aae7d4472b4c0
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 15:19:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
 
 def prime?(n)
-  i = 0
+  i = 1
+  result = true
   n *= -1 if n < 0 
-  while i < 
-    
-    
-  
+  while i < n / 2 #ideally while i < sqrt of n
+    n % i == 0 ? result == false : i += 1
+  end
+  result
 end
